a55ba751f05e076ce7bb73d407eea2c789bb8a59
Priyam <pn613@nyu.edu>
2014-10-23 00:24:16 -0500

    Changes Suggested

--- build_component.py
+++ build_component.py
@@ -228,9 +228,12 @@ def help_exit(errMsg, parser):
 
 def main():
   # Parse the options provided. 
-  helpstring = "python preparetest.py [-t] [-v] [-c] [-r] <target>"
+  helpstring = "python preparetest.py [-t] [-v] [-c] [-r] [-a] <target>"
   parser = optparse.OptionParser(usage=helpstring)
-
+  # add a option about copying over file trees to the target dir
+  parser.add_option("-a", "--trees", action="store_true",
+      dest="copy_trees", default=False,
+      help="Copy over file trees to the target dir ")
   parser.add_option("-t", "--testfiles", action="store_true",
       dest="include_tests", default=False,
       help="Include files required to run the unit tests ")
@@ -268,6 +271,7 @@ def main():
   print "Building into", target_dir
 
   # Set variables according to the provided options.
+  copytree = options.copy_trees
   repytest = options.include_tests
   RANDOMPORTS = options.randomports
   verbose = options.verbose
@@ -276,7 +280,7 @@ def main():
 
   # This script's parent directory is the root dir of all dependent 
   # repositories, path/to/component/DEPENDENCIES/ 
-  repos_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  repos_root_dir = os.path.dirname(os.path.abspath(__file__))
 
   # Set working directory to the target
   os.chdir(target_dir)	
@@ -304,53 +308,50 @@ def main():
   #   "." with the sources of the component we want to build,
   #   "scripts/" with the build config file, and
   #   "DEPENDENCIES/" with all the dependent repos.
-  os.chdir(os.path.join(repos_root_dir, ".."))
-
+  #os.chdir(os.path.join(repos_root_dir, ".."))
+  os.chdir(repos_root_dir)
   # Copy the necessary files to the respective target folders, 
   # following the instructions in scripts/config_build.txt.
-  config_file = open("scripts/config_build.txt")  
+  config_file = open("scripts/config_build.txt")
+  
   for line in config_file.readlines():
     # Ignore comments and blank lines
     if line.startswith("#") or line.strip() == '':
       continue
 
-      # If there are subdirectories, handle them
-      # If there are subdirectories, handle them
-    subdir = string[string.find("*" + 1):]
-       if subdir == "":
-         copy_to_target(file_expr, target)
-       else:
-	 target_dir = target_dir + "\\" + subdir
-	 copy_tree_to_target(source_spec, target_dir)
-      
-             
     # Anything non-comment and non-empty specifies a 
     # source file or directory for us to use.
     if line.startswith("test"):
       # Build instructions for unit tests look like this:
       # "test ../relative/path/to/required/file_or_fileglob"
       if repytest:
-        source_spec = line.split(" ", 1)[1].strip()
+        source_spec = line.split()[1]
+        sub_target_dir = line.split()[2]
       else:
         # Tests weren't requested. Skip.
         continue
-      
-      if line.startswith("tree"):
-              # What to do if we have a tree
-              # "tree ../sample/dir/names
-              source_spec = line.split(" ", 1)[1].strip()
-              
-              copy_tree_to_target(source_spec, target_dir)
+    elif line.startswith("tree"):
+      if copytree:
+        source_spec = line.split()[1]
+        sub_target_dir = line.split()[2]
+      else:
+        # Trees weren't requested. Skip.
+        continue
     else:
       # This is a non-test instruction.
-      source_spec = line.strip()
-
-      # Dont copy to target if tree is already at target
-      if line.starts_with("tree"):
-              continue
-
-      copy_to_target(source_spec, target_dir)
-
+      source_spec = line.split()[0]
+      sub_target_dir =  line.split()[1]
+    
+  os.chdir(target_dir)
+  if not os.path.exists(sub_target_dir):
+    os.makedirs(sub_target_dir)
+    
+  os.chdir(os.path.join(repos_root_dir, ".."))
+  if copytree:
+    copy_tree_to_target(source_spec, target_dir + '/' + sub_target_dir)   
+  else:
+    copy_to_target(source_spec, target_dir + '/' + sub_target_dir)
+ 
   
   # Set working directory to the target
   os.chdir(target_dir)
